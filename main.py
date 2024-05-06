1d98a6ff37c18da1799c4c873fe74e4e7d53c1a3
chitwan982 <118850532+chitwan982@users.noreply.github.com>
2024-05-06 21:54:20 +0530

    main.py

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 from flask import Flask
+from flask import render_template
+from flask import jsonify, request
 
 webapp=Flask(__name__)
 
 @webapp.route("/")
 def minimal_f():
     return "<p>This is Minimal Application in Flask!</p>"
+
+@webapp.route("/routing")
+def routing():
+    return "<p>This is routing!</p>"
+
+@webapp.route("/templates")
+def templates():
+    return render_template("index.html")
+
+
+
+@webapp.route("/API/flavours/<flavour_name>")
+def ice_cream_GET(flavour_name):
+    result={
+        "Flavour": f'{flavour_name}'
+    }
+    return jsonify(result)
+
 
 webapp.run(debug=True)
